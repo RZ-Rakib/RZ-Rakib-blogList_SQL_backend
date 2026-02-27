d1585250d533fc6c421def6b620610cac4328f3c
Md Rakibuzzaman Rakib <rakibzaman724@gmail.com>
2026-02-27 13:31:20 +0200

    blog model and several routes have created

--- backend/cli.js
+++ backend/cli.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
+app.use(express.json())
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
@@ -11,17 +14,72 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   }
 })
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    const blogs = await sequelize.query('SELECT * FROM blogs', { type: QueryTypes.SELECT })
-    blogs.forEach(blog => {
-      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
-    })
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  },
+  date: {
+    type: DataTypes.DATE
   }
-}
+}, {
+  sequelize,
+  underscored:true,
+  timestamps: false,
+  modelName: 'blog'
+})
+Blog.sync()
 
-main()
+app.get('/api/blogs', async (req, res) => {
+  try {
+    const blogs  = await Blog.findAll()
+    res.json(blogs)
+  } catch {
+    res.status(404).end()
+  }
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create({ ...req.body, date: new Date() })
+    res.json(blog)
+  } catch {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res ) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id)
+    if(!blog){
+      res.status(404).json({ message: 'blog not found' })
+    }
+
+    await blog.destroy()
+    res.status(204).end()
+  } catch {
+    res.status(404).end()
+  }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
